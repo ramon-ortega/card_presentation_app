f1709bdd16b321f1e87559f594ac9b8247c9c05e
Ramon de Jesus Ortega <ramortega1594@gmail.com>
2023-02-25 19:44:12 -0600

    Final project

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -29,23 +30,41 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
           SafeArea(
             child: selectedIndex == 0
-                ? ListView(
-                    children: const [
-                      _Header(),
-                      _DataInfo(),
-                    ],
+                ? Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: ListView(
+                      children: const [
+                        SizedBox(
+                          height: 30,
+                        ),
+                        _Header(),
+                        _DataInfo(),
+                        _ProjectsSection()
+                      ],
+                    ),
                   )
                 : ListView(
                     children: [
-                      const Text(
-                        'Cambia el fondo',
-                        style: TextStyle(
-                          fontSize: 20,
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      Center(
+                        child: Text(
+                          'CAMBIAR TEMA',
+                          style: GoogleFonts.sourceSansPro(
+                            textStyle: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
                         ),
                       ),
+                      const SizedBox(
+                        height: 20,
+                      ),
                       Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
                         children: [
                           _ButtonTheme(
                               imageUrl: 'assets/background1.jpg',
@@ -67,6 +86,11 @@ class _HomeScreenState extends State<HomeScreen> {
                           ),
                         ],
                       ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      const _ContactSection(),
+                      const _SoftSkillsSection(),
                     ],
                   ),
           ),
@@ -90,6 +114,169 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ],
       ),
+    );
+  }
+}
+
+class _SoftSkillsSection extends StatelessWidget {
+  const _SoftSkillsSection({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 20,
+          ),
+          Text(
+            'SOFT SKILLS',
+            style: GoogleFonts.sourceSansPro(
+              textStyle: const TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Card(
+            child: ListTile(
+              leading: const Icon(
+                Icons.voice_chat,
+                color: Colors.teal,
+              ),
+              title: const Text('Comunicación'),
+              trailing: Image.asset('assets/comunicacion.png'),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              leading: const Icon(
+                Icons.check_box_outlined,
+                color: Colors.teal,
+              ),
+              title: const Text('Responsabilidad'),
+              trailing: Image.asset('assets/responsable.png'),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              leading: const Icon(
+                Icons.voice_chat,
+                color: Colors.teal,
+              ),
+              title: const Text('Trabajo en Equipo'),
+              trailing: Image.asset('assets/equipo.jpg'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _ContactSection extends StatelessWidget {
+  const _ContactSection({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          'CONTACTO',
+          style: GoogleFonts.sourceSansPro(
+            textStyle: const TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        ConstrainedBox(
+          constraints: const BoxConstraints(
+            maxHeight: 150,
+            minWidth: 150,
+          ),
+          child: Image.asset('assets/qr-code.png'),
+        )
+      ],
+    );
+  }
+}
+
+class _ProjectsSection extends StatelessWidget {
+  const _ProjectsSection({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const SizedBox(
+          height: 20,
+        ),
+        Text(
+          'PROYECTOS',
+          style: GoogleFonts.sourceSansPro(
+            textStyle: const TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        GridView.count(
+          primary: false,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+          crossAxisCount: 2,
+          physics: const NeverScrollableScrollPhysics(),
+          shrinkWrap: true,
+          children: <Widget>[
+            const Card(
+              color: Colors.black,
+              child: Image(
+                image: AssetImage('assets/wemerang.png'),
+              ),
+            ),
+            Card(
+              color: Colors.grey[300],
+              child: const Image(
+                image: AssetImage('assets/simapaj.png'),
+              ),
+            ),
+            Card(
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(5),
+                child: const Image(
+                  image: AssetImage('assets/kulhman.jpg'),
+                ),
+              ),
+            ),
+            Card(
+              color: Colors.black,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(5),
+                child: const Image(
+                  image: AssetImage('assets/banner.jpeg'),
+                ),
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+      ],
     );
   }
 }
@@ -121,39 +308,46 @@ class _ButtonTheme extends StatelessWidget {
 }
 
 class _DataInfo extends StatelessWidget {
-  const _DataInfo({
-    super.key,
-  });
+  const _DataInfo({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Column(
-        children: [
-          ListTile(
+    return Column(
+      children: const [
+        Card(
+          child: ListTile(
             leading: Icon(
               Icons.phone,
               color: Colors.teal,
             ),
             title: Text('+52 492 795 4036'),
           ),
-          ListTile(
+        ),
+        Card(
+          child: ListTile(
             leading: Icon(
               Icons.email_outlined,
               color: Colors.teal,
             ),
             title: Text('ramortega1594@gmail.com'),
           ),
-        ],
-      ),
+        ),
+        Card(
+          child: ListTile(
+            leading: Icon(
+              Icons.location_city,
+              color: Colors.teal,
+            ),
+            title: Text('Zacatecas, México'),
+          ),
+        ),
+      ],
     );
   }
 }
 
 class _Header extends StatelessWidget {
-  const _Header({
-    super.key,
-  });
+  const _Header({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -171,19 +365,34 @@ class _Header extends StatelessWidget {
             ),
           ),
         ),
-        const Text(
+        const SizedBox(
+          height: 10,
+        ),
+        Text(
           'Ramón de Jesús Ortega',
-          style: TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
+          style: GoogleFonts.roboto(
+            textStyle: const TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
           ),
         ),
-        const Text(
+        const SizedBox(
+          height: 10,
+        ),
+        Text(
           'FLUTTER DEVELOPER',
-          style: TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
+          style: GoogleFonts.sourceSansPro(
+            textStyle: const TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
           ),
+        ),
+        const SizedBox(
+          height: 20,
         ),
       ],
     );
